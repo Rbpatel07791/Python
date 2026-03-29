575c25b1dc6c3efb4cdb6f0cd8ea5d2305cf450d
Rbpatel07791 <rikkipatel077@gmail.com>
2026-03-29 11:13:34 -0400

    Create comment.py

---
+++ comment.py
@@ -0,0 +1,8 @@
+#commient 
+# This is a comment in Python. Comments are ignored by the interpreter and are used to explain code or provide additional information.
+# You can write single-line comments using the '#' symbol.  
+# Comments can also be used to temporarily disable code. For example:
+# print("This line will not be executed.")
+# You can also write multi-line comments using triple quotes (''' or """).
+''' this is a multi-line comment that can span multiple lines. It is often used for docstrings, which are used to document functions, classes, and modules. '''
+# Remember to use comments wisely to improve code readability and maintainability.
